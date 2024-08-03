8f70c66a5b1c0bda73a363afa31e1684b11cf7c2
pijiamtech <pijiamtech@gmail.com>
2024-08-03 17:21:02 +0700

    test exam

--- test.py
+++ test.py
@@ -1,17 +1,22 @@
-import pytesseract
-from PIL import Image, ImageGrab
+# import pytesseract
+# from PIL import Image, ImageGrab
 
-def trim_str(raw_txt):
-    new_txt = ""
-    for i in range(len(raw_txt)):
-        if ord(raw_txt[i]) != 32:
-            new_txt += raw_txt[i]
-        elif ord(raw_txt[i+1]) < 3585:
-            new_txt += raw_txt[i]
-    return new_txt
+# def trim_str(raw_txt):
+#     new_txt = ""
+#     for i in range(len(raw_txt)):
+#         if ord(raw_txt[i]) != 32:
+#             new_txt += raw_txt[i]
+#         elif ord(raw_txt[i+1]) < 3585:
+#             new_txt += raw_txt[i]
+#     return new_txt
 
-if __name__ == "__main__":
-    img = Image.open('1113.png')
-    result = pytesseract.image_to_string(img, lang="tha+eng")
-    new_txt = trim_str(result)
-    print(new_txt)
+# if __name__ == "__main__":
+#     img = Image.open('1113.png')
+#     result = pytesseract.image_to_string(img, lang="tha+eng")
+#     new_txt = trim_str(result)
+#     print(new_txt)
+
+def unicode_table():
+    for i in range(ord("ก"), ord("ฮ") + 1):
+        print("{0} : {0:#x} : {0:c}".format(i))
+unicode_table()
